fa8a7ccc2d8cd82cdc632bb51d21d9c39ad3320d
Mike Bagwell <lowghost.clone@gmail.com>
2022-11-26 21:38:52 -0500

    mock examples

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -16,6 +16,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-storysource",
+    "storybook-addon-mock",
   ],
   framework: "@storybook/react",
   core: {
