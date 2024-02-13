8843bfbed4d140ad300893b52450ab254c6764c4
Mike Bagwell <lowghost.clone@gmail.com>
2024-02-13 10:48:19 -0500

    ran scripts, renamed problematic mdx for now

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,22 +3,18 @@ const path = require("path");
 
 module.exports = {
   stories: ["../stories/**/*.stories.@(mdx|js|jsx|ts|tsx)"],
-  addons: [
-    {
-      // eslint-disable-next-line storybook/no-uninstalled-addons
-      name: "@storybook/addon-docs",
-      options: {
-        transcludeMarkdown: true,
-        sourceLoaderOptions: {
-          injectStoryParameters: false,
-        },
+
+  addons: [{
+    // eslint-disable-next-line storybook/no-uninstalled-addons
+    name: "@storybook/addon-docs",
+    options: {
+      transcludeMarkdown: true,
+      sourceLoaderOptions: {
+        injectStoryParameters: false,
       },
     },
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-storysource",
-    "storybook-addon-mock",
-  ],
+  }, "@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-storysource", "storybook-addon-mock", "@storybook/addon-mdx-gfm"],
+
   webpackFinal: async (config) => {
     // I don't want to minimize because I'm running some literate testing
     // through UnitTest and, for example's sake, have some that don't use
@@ -37,8 +33,14 @@ module.exports = {
     };
     return config;
   },
-  framework: "@storybook/react",
-  core: {
-    builder: "webpack5",
-  },
+
+  framework: {
+    name: "@storybook/react-webpack5",
+
+    options: {
+      builder: {
+        useSWC: true
+      }
+    }
+  }
 };
